a3f26fc98a7cb8f0a3dfebdc8744c0a6d0f80173
Adittya Dey <adittya.dey@g.bracu.ac.bd>
2026-02-02 22:30:33 +0600

    Update classroom.sql
    Practice

--- classroom.sql
+++ classroom.sql
@@ -1,5 +1,17 @@
-create database temp1;
-CREATE DATABASE temp2;
-create database college;
-drop database temp1;
-drop database temp2;
+
+-- 2. Create it fresh
+CREATE DATABASE IF NOT EXISTS college; 
+
+-- 3. Tell MySQL to use it
+USE college; 
+CREATE TABLE student(
+rollno INT PRIMARY KEY,
+name VARCHAR(50)
+);
+INSERT INTO student
+(rollno, name)
+values
+(101, "DADDY"),
+(100, "FATHER"),
+(99, "ABBA");
+
